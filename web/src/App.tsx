e5e65e64de86829d7e7ece57180110edda053f63
Eugene Chekan <eugene.chekan@gmail.com>
2026-08-27 22:36:24 +0200

    feat: wire /admin into the route table

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,10 +6,13 @@ import { ApiProvider } from './api/ApiProvider'
 import { HttpLibraApi } from './api/HttpLibraApi'
 import { createQueryClient } from './queryClient'
 import { routes } from './routes'
+import { AdminLayout } from './screens/AdminLayout'
+import { AdminUsersScreen } from './screens/AdminUsersScreen'
 import { BookScreen } from './screens/BookScreen'
 import { LibraryScreen } from './screens/LibraryScreen'
 import { ShelvesScreen } from './screens/ShelvesScreen'
 import { LoginScreen } from './screens/LoginScreen'
+import { RequireAdmin } from './session/RequireAdmin'
 import { RequireSession } from './session/RequireSession'
 import { SessionProvider } from './session/SessionProvider'
 import { AppShell } from './shell/AppShell'
@@ -28,6 +31,12 @@ export function AppRoutes() {
           <Route path={routes.reader} element={<ReaderScreen />} />
           <Route path={routes.shelves} element={<ShelvesScreen />} />
           <Route path={routes.chat} element={<ChatScreen />} />
+          <Route element={<RequireAdmin />}>
+            <Route element={<AdminLayout />}>
+              <Route path={routes.admin} element={<Navigate to={routes.adminUsers} replace />} />
+              <Route path={routes.adminUsers} element={<AdminUsersScreen />} />
+            </Route>
+          </Route>
           <Route path="*" element={<NotFoundScreen />} />
         </Route>
       </Route>
